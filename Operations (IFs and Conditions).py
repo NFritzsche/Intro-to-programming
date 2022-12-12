1b98d5135361948c745d9e73c0762841afb5a206
nicholasfritzsche <nicholas.fritzsche@sdsw.org>
2022-12-12 09:47:52 -0500

    lessons

---
+++ Operations (IFs and Conditions).py
@@ -0,0 +1,12 @@
+x = 2
+print(x)
+x = x + 5
+print(x)
+x = 2
+x +=5
+print(x)
+y = 3
+x = y**3
+print(x)
+z = y**2 + (5+x) +1
+print(z)
---
+++ strings.py
@@ -0,0 +1,5 @@
+team = "Bayern Munich"
+print(team.upper())
+print(team.lower())
+print(team.find('r'))
+print(team.replace('Munich','2'))
